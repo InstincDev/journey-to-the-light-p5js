b8384662d7ea823d6a18fbd06425f602a4ae7061
ChanelT <crene7722@gmail.com>
2021-12-06 07:54:08 -0600

    added comments

--- task1/sketch.js
+++ task1/sketch.js
@@ -16,12 +16,14 @@ function draw() {
   background(0);
   blendMode(ADD);
 
-
-let force = createVector(0, 0);
+  let force = createVector(0, 0);
   let wind = createVector(0, 0);
- checkTime();
 
-  if(counter > 1){
+  // check how long the animation has been running
+  checkTime();
+
+  // move light
+  if (counter > 1) {
     for (let light of lights) {
       light.emit(1);
       light.applyForce(force);
@@ -30,25 +32,26 @@ let force = createVector(0, 0);
       light.update();
       light.move();
     }
-  } else{
-    // lights.push(new Light(200, 200));
-      for (let light of lights) {
-        light.pos = createVector(width/2,height/2);
-        light.emit(1);
-        light.applyForce(force);
-        light.applyForce(wind);
-        light.show();
-        light.update();
-        // light.move();
-      }
+  }
+  // stop light and move to center of screen
+  else {
+    for (let light of lights) {
+      light.pos = createVector(width / 2, height / 2);
+      light.emit(1);
+      light.applyForce(force);
+      light.applyForce(wind);
+      light.show();
+      light.update();
+      // light.move();
     }
-
-  
+  }
 }
 
-function checkTime(){
-  if(frameCount % 60 == 0 && counter > 0){
-    console.log(counter)
-  counter --;
+function checkTime() {
+  // use frame count modulo of 60 (1 second)
+  // and counter to track time
+  if (frameCount % 60 == 0 && counter > 0) {
+    // console.log(counter);
+    counter--;
   }
 }
