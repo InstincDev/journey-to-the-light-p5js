5b49a8edbcd72dcb5255f4c8b115d127099d091e
ChanelT <crene7722@gmail.com>
2021-12-06 07:47:47 -0600

    added frame count timer

--- task1/sketch.js
+++ task1/sketch.js
@@ -1,5 +1,6 @@
 let lights = [];
 let img;
+let counter = 5;
 
 function preload() {
   img = loadImage("texture.png");
@@ -15,15 +16,39 @@ function draw() {
   background(0);
   blendMode(ADD);
 
-  let force = createVector(0, 0);
-  let wind = createVector(0, 0);
 
-  for (let light of lights) {
-    light.emit(1);
-    light.applyForce(force);
-    light.applyForce(wind);
-    light.show();
-    light.update();
-    light.move();
+let force = createVector(0, 0);
+  let wind = createVector(0, 0);
+ checkTime();
+
+  if(counter > 1){
+    for (let light of lights) {
+      light.emit(1);
+      light.applyForce(force);
+      light.applyForce(wind);
+      light.show();
+      light.update();
+      light.move();
+    }
+  } else{
+    // lights.push(new Light(200, 200));
+      for (let light of lights) {
+        light.pos = createVector(width/2,height/2);
+        light.emit(1);
+        light.applyForce(force);
+        light.applyForce(wind);
+        light.show();
+        light.update();
+        // light.move();
+      }
+    }
+
+  
+}
+
+function checkTime(){
+  if(frameCount % 60 == 0 && counter > 0){
+    console.log(counter)
+  counter --;
   }
 }
